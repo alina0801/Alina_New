6278d1a5a8770b82772f0db8123285f12baba01c
alina0801 <adjz123@yahoo.com>
2023-10-17 14:08:59 -0400

    first commit

--- src/day1_practice/GroupClass.java
+++ src/day1_practice/GroupClass.java
@@ -3,5 +3,6 @@ package day1_practice;
 public class GroupClass {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("New change");
     }
 }
